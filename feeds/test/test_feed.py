37c52cf55b075725a75d150b7b8aaeb505262510
dryad <67245619+dryad@users.noreply.github.com>
2023-02-11 13:54:43 -0700

    added simple functionality to Test data feed

--- feeds/test/test_feed.py
+++ feeds/test/test_feed.py
@@ -8,7 +8,9 @@ class Test(DataFeed):
     NAME = 'test'
     ID = 0
     HEARTBEAT = 1
+    START_TIME = None
     #Feed-specific class-level attrs
 
-    def get_data_point(cls):
-        return random.rand()
+    # @classmethod
+    # def get_data_point(cls):
+    #     return random.rand()
