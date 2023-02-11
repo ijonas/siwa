dd8040bcb06cadebb9ce530b2733cdc6ae7e1949
dryad <67245619+dryad@users.noreply.github.com>
2023-02-11 13:54:06 -0700

    replaced get_most_recently_stored_data_point with three more modular functions, and cleaned up try/except clauses and silent error handling

--- feeds/data_feed.py
+++ feeds/data_feed.py
@@ -7,7 +7,7 @@ import constants as c
 from threading import Lock
 from datetime import datetime, timezone
 from dataclasses import dataclass
-
+ 
 # logging.StreamHandler()
 # ch.setLevel(logging.DEBUG)
 # formatter = logging.Formatter('%(asctime)s:%(thread)d - %(name)s - %(levelname)s - %(message)s')
@@ -33,6 +33,7 @@ class DataFeed:
     START_TIME: datetime
     ACTIVE: bool = False
     COUNT: int = 0              #number of data points served since starting
+    DATA_KEYS = (c.FEED_NAME, c.TIME_STAMP, c.DATA_POINT)
 
 
     @classmethod
@@ -60,43 +61,70 @@ class DataFeed:
                 datafile.write(cls.format_data(dp))
 
     @classmethod
+    def get_last_csv_line(cls):
+        if not os.path.exists(cls.get_data_dir()):
+            return None
+        
+        with open(cls.get_data_dir(), 'rb') as datafile:
+            datafile.seek(-2, os.SEEK_END)
+            while datafile.read(1) != f'{c.LINE_START}'.encode(): #needs to be in bytes
+                datafile.seek(-2, os.SEEK_CUR)
+
+            return datafile.readline().decode("utf-8").replace(c.LINE_START, '')
+
+    @classmethod
+    def parse_csv_line(cls, line):
+        line = line.split(',')
+        timestamp = datetime.strptime(line[0], c.DATEFORMAT)
+        data_point = float(line[1]) #TODO: are floats OK? decimal type needed?
+        return timestamp, data_point
+
+    @classmethod
+    def line_to_dict(cls, line):
+        return dict(zip(cls.DATA_KEYS, line)) 
+
+    @classmethod
     def get_most_recently_stored_data_point(cls):
         '''return most recently stored-to-disk data point;
         returns dict of {feedname, timestamp, datapoint}'''
-        data_point = None
+        line = (cls.NAME,) + cls.parse_csv_line(cls.get_last_csv_line())
+        return dict(zip(cls.DATA_KEYS, line))
 
-        try:
-            with open(cls.get_data_dir(), 'r') as datafile:
-                try:
-                    #find only last line of CSV, which may be huge
-                    datafile.seek(-2, os.SEEK_END)
-                    while datafile.read(1) != b'\n':
-                        datafile.seek(-2, os.SEEK_CUR)
+        # return cls.line_to_dict(
+        # data_point = None
 
-                except OSError:
-                    # handle case csv file is new and has only 1 line
-                    datafile.seek(0)
+        # try:
+        #     with open(cls.get_data_dir(), 'r') as datafile:
+        #         try:
+        #             #find only last line of CSV, which may be huge
+        #             datafile.seek(-2, os.SEEK_END)
+        #             while datafile.read(1) != b'\n':
+        #                 datafile.seek(-2, os.SEEK_CUR)
 
-                last_csv_line = datafile.readline()
-                if last_csv_line:
-                    # if csv file not blank:
-                    data_values = last_csv_line.split(', ')
-                    data_point = {'feedname':cls.NAME,
-                                #output same format;
-                                #csv would be easier to parse if no comma in c.DATEFORMAT
-                                'timestamp':', '.join(data_values[:2]),
+        #         except OSError:
+        #             # handle case csv file is new and has only 1 line
+        #             datafile.seek(0)
 
-                                #alternatively...
-                                #'timestamp':datetime.strptime(', '.join(data_values[:2]), c.DATEFORMAT),
-                                #TODO: are floats OK? decimal type needed?
-                                'data_point':float(data_values[2])}
-            return data_point
-        except Exception as e:
-            #if file doesnt exist:
-            return data_point
+        #         last_csv_line = datafile.readline()
+        #         if last_csv_line:
+        #             # if csv file not blank:
+        #             data_values = last_csv_line.split(', ')
+        #             data_point = {'feedname':cls.NAME,
+        #                         #output same format;
+        #                         #csv would be easier to parse if no comma in c.DATEFORMAT
+        #                         'timestamp':', '.join(data_values[:2]),
+
+        #                         #alternatively...
+        #                         #'timestamp':datetime.strptime(', '.join(data_values[:2]), c.DATEFORMAT),
+        #                         #TODO: are floats OK? decimal type needed?
+        #                         'data_point':float(data_values[2])}
+        #     return data_point
+        # except Exception as e:
+        #     #if file doesn't exist:
+        #     return data_point
 
     @staticmethod
     def format_data(dp):
         timenow =  datetime.now(timezone.utc)
         strtime = timenow.strftime(c.DATEFORMAT)
-        return f'{strtime}, {dp}, \n'
+        return f'{c.LINE_START}{strtime},{dp},\n'
