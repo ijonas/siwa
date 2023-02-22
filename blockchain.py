31ea5312d084e10c4a0b62f9f8aa0ba473815f31
dryad <67245619+dryad@users.noreply.github.com>
2023-02-22 11:38:43 -0700

    cleanup

--- blockchain.py
+++ blockchain.py
@@ -5,9 +5,9 @@ from web3 import Web3
 
 
 
-arbi_goerli = Web3(Web3.HTTPProvider(c.ARBITRUM_GOERLI))
-arbi_main = Web3(Web3.HTTPProvider(c.ARBITRUM_MAINNET))
-eth_main = Web3(Web3.HTTPProvider(c.ETHEREUM_MAINNET))
+arbi_goerli = Web3(Web3.HTTPProvider(c.ARBITRUM_GOERLI_RPC))
+arbi_main = Web3(Web3.HTTPProvider(c.ARBITRUM_MAINNET_RPC))
+eth_main = Web3(Web3.HTTPProvider(c.ETHEREUM_MAINNET_RPC))
 
 def contract_interface(provider, address, abi):
     return provider.eth.contract(address=address, abi=abi)
@@ -19,13 +19,21 @@ class Pokt():
     ...
 
 
-class Translucent(Pokt):
+class Translucent:
 
     '''A class to collect Translucent functionality
     '''
     gauss_arbi_goerli  = contract_interface(
             arbi_goerli,
-            address=c.TRANSLUCENT_GAUSS_GOERLI_ARBITRUM,
+            address=c.TRANSLUCENT_GAUSS_ARBITRUM_GOERLI,
             abi=c.TRANSLUCENT_FLUX_AGGREGATOR
         )
+    #currently unsupported
+    # gauss_arbi_main  = contract_interface(
+    #         arbi_main,
+    #         address=c.TRANSLUCENT_GAUSS_ARBITRUM_MAINNET,
+    #         abi=c.TRANSLUCENT_FLUX_AGGREGATOR
+    #     )
 
+
+        
