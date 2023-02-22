05e58530a80a6f64c0b5295cb671f0a54aa5b0d9
dryad <67245619+dryad@users.noreply.github.com>
2023-02-22 11:46:42 -0700

    gauss feed reading data from chain now

--- feeds/gauss/gauss.py
+++ feeds/gauss/gauss.py
@@ -12,12 +12,11 @@ from feeds.data_feed import DataFeed
 from blockchain import Translucent
 
 class Gauss(DataFeed):
-    #set the chain for Gauss operation here
     CHAIN = c.ARBITRUM_GOERLI
-
     NAME = 'gauss'
     ID = 1
     HEARTBEAT = 1
+    DATAPOINT_DEQUE = deque([], maxlen=100)
     #Feed-specific class-level attrs
     PERCENT = .01
     VOLATILITY = 1
@@ -35,7 +34,10 @@ class Gauss(DataFeed):
 
     @classmethod
     def process_source_data_into_siwa_datapoint(cls, source_data):
-        ''' todo docstring // explain function '''
+        ''' We have a dynamic standard deviation, based on the last data point, so we can get a new data point
+            that is within a certain percentage of the last data point. Without this, if the previous data point
+            is large, the delta (between previous and new) will be very small, and vice versa.
+        '''
         std = max(cls.VOLATILITY * source_data * cls.PERCENT, .001)
         delta = random.normal(0, std)
         return source_data + cls.VOLATILITY * delta
@@ -44,9 +46,7 @@ class Gauss(DataFeed):
     def create_new_data_point(cls):
         ''' fetches data from datasource,
             applies siwa algorithms to create new siwa datapoint,
-            returns said new siwa datapoint (or None if datasource stale?? TBD) '''
-
-        '''
+            returns said new siwa datapoint (or None if datasource stale?? TBD)
         NOTE:
             This method requires the last data point of the actual feed deployed on the blockchain
             This is because the variance of the distribution (determining the next data point) is a function of the last data point
@@ -54,4 +54,5 @@ class Gauss(DataFeed):
         #NOTE TODO / BUG POTENTIAL / should we check and ensure latest datasource data
         #is new and we haven't seen it before? Or is that irrelevant?
         source_data = cls.get_latest_source_data()
+        # print(f'got source data and it is {source_data}') #manually checking functionality
         return cls.process_source_data_into_siwa_datapoint(source_data)
