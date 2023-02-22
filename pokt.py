26fb8f78973c011a78f8f9c2efc6f6c69d6526e9
dryad <67245619+dryad@users.noreply.github.com>
2023-02-22 11:05:23 -0700

    cleanup

--- pokt.py
+++ pokt.py
@@ -3,35 +3,28 @@ import constants as c
 from numpy import random
 from web3 import Web3
 
+
+
+arbi_goerli = Web3(Web3.HTTPProvider(c.ARBITRUM_GOERLI))
+arbi_main = Web3(Web3.HTTPProvider(c.ARBITRUM_MAINNET))
+eth_main = Web3(Web3.HTTPProvider(c.ETHEREUM_MAINNET))
+
+def contract_interface(provider, address, abi):
+    return provider.eth.contract(address=address, abi=abi)
+
 #we use Pokt to access chain data
 class Pokt():
     '''A class to collect Pokt functionality
     '''
-
-    ARBITRUM = 'https://goerli-rollup.arbitrum.io/rpc' #f'https://{c.POKT_ARBITRUM}.gateway.pokt.network/v1/lb/{c.POKT_PORTAL_ID}'
-    ETHEREUM = f'https://{c.POKT_ETHEREUM}.gateway.pokt.network/v1/lb/{c.POKT_PORTAL_ID}'
-
-
-
-
-    A = Web3(Web3.HTTPProvider(ARBITRUM))
-    E = Web3(Web3.HTTPProvider(ETHEREUM))
-
-    @staticmethod
-    def contract_interface(provider, address, abi):
-        return provider.eth.contract(address=address, abi=abi)
-
+    ...
 
 
 class Translucent(Pokt):
 
-    gauss_test  = Pokt.contract_interface(
-            Pokt.A,
-            address=c.TRANSLUCENT_GAUSS_GOERLI_ARBITRUM,
-            abi=c.TRANSLUCENT_FLUX_AGGREGATOR
-        )
-    gauss_test_E  = Pokt.contract_interface(
-            Pokt.E,
+    '''A class to collect required Translucent functionality
+    '''
+    gauss_arbi_goerli  = contract_interface(
+            arbi_goerli,
             address=c.TRANSLUCENT_GAUSS_GOERLI_ARBITRUM,
             abi=c.TRANSLUCENT_FLUX_AGGREGATOR
         )
