f4d4042ce4e383d631c2a40ae610a25a5fdbb05d
dryad <67245619+dryad@users.noreply.github.com>
2023-03-12 22:07:03 -0600

    clean class structure for stablecoin feeds

--- feeds/stablecoins/stablecoins.py
+++ feeds/stablecoins/stablecoins.py
@@ -1,47 +1,52 @@
 from feeds.data_feed import DataFeed
 import apis.coingecko as cgecko
 from collections import deque 
+import constants as c
 
 
-class USDC(DataFeed):
+class StableCoin(DataFeed):
+    @classmethod
+    def process_source_data_into_siwa_datapoint(cls):
+        '''
+        '''
+        market_data = cgecko.fetch_data_from_web([cls.CGECKO_ID])
+        if market_data is None:
+            return cls.DATAPOINT_DEQUE[-1]
+        px = market_data[cls.CGECKO_ID][c.PRICE]
+        return px 
+
+    @classmethod
+    def create_new_data_point(cls):
+        return cls.process_source_data_into_siwa_datapoint()
+
+
+
+class USDC(StableCoin):
     NAME = 'usdc'
     ID = 3 
     HEARTBEAT = 120 
     DATAPOINT_DEQUE = deque([], maxlen=100)
-    CGECKO_ID = 'usd-coin'
-
-    @classmethod
-    def process_source_data_into_siwa_datapoint(cls):
-        '''
-        '''
-        market_data = cgecko.fetch_data_from_web(cls.CGECKO_ID)) 
-        if market_data is None:
-            return cls.DATAPOINT_DEQUE[-1]
-        breakpoint()
-        return res
-
-    @classmethod
-    def create_new_data_point(cls):
-        return cls.process_source_data_into_siwa_datapoint()
+    CGECKO_ID = c.USDC
 
 
-
-class BUSD(DataFeed):
+class BUSD(StableCoin):
     NAME = 'busd'
     ID = 4 
     HEARTBEAT = 120 
     DATAPOINT_DEQUE = deque([], maxlen=100)
-    CGECKO_ID = 'binance-usd'
+    CGECKO_ID = c.BUSD
 
-    @classmethod
-    def process_source_data_into_siwa_datapoint(cls):
-        '''
-        '''
-        market_data = cgecko.fetch_data_from_web(cls.CGECKO_ID)) 
-        if market_data is None:
-            return cls.DATAPOINT_DEQUE[-1]
-        return res
+class Tether(StableCoin):
+    NAME = 'tether'
+    ID = 5 
+    HEARTBEAT = 120 
+    DATAPOINT_DEQUE = deque([], maxlen=100)
+    CGECKO_ID = c.TETHER
 
-    @classmethod
-    def create_new_data_point(cls):
-        return cls.process_source_data_into_siwa_datapoint()
+
+class Dai(StableCoin):
+    NAME = 'dai'
+    ID = 6 
+    HEARTBEAT = 120 
+    DATAPOINT_DEQUE = deque([], maxlen=100)
+    CGECKO_ID = c.DAI
