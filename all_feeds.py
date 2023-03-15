a139a71a97cbe500b90e4e0523e22e2b6d45ace1
dryad <67245619+dryad@users.noreply.github.com>
2023-03-15 00:40:28 -0600

    added DogCoins

--- all_feeds.py
+++ all_feeds.py
@@ -1,11 +1,11 @@
 from feeds.gauss import gauss
-from feeds.crypto_indices import mcap1000 
+from feeds.crypto_indices import mcap1000, dogcoins
 from feeds.stablecoins import stablecoins as s 
 from feeds import test_feed
 
 Test = test_feed.Test
 Gauss = gauss.Gauss
-MCAP1000 = mcap1000.MCAP1000
+MCAP1000, DogCoins = mcap1000.MCAP1000, dogcoins.DogCoins
 USDC, BUSD, Tether, Dai = s.USDC, s.BUSD, s.Tether, s.Dai
 
 #NOTE: this is a dict of all feed classes that SIWA can run, keyed by feed name
@@ -13,9 +13,10 @@ USDC, BUSD, Tether, Dai = s.USDC, s.BUSD, s.Tether, s.Dai
 #
 #TO ENABLE OR DISABLE A FEED, ADD OR REMOVE IT FROM THIS DICT  
 all_feeds = {
-    Gauss.NAME: Gauss, 
+    # Gauss.NAME: Gauss, 
     Test.NAME: Test,
     MCAP1000.NAME: MCAP1000,
+    DogCoins.NAME: DogCoins,
     USDC.NAME: USDC, 
     BUSD.NAME: BUSD,
     Tether.NAME: Tether,
