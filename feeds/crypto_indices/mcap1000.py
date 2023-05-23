0ef27c9cda9dbaf9e5a5449c96b8c94ac8480020
deepsp94 <anantsp94@gmail.com>
2023-05-23 12:02:48 +0530

    Aggregate from multiple sources

--- feeds/crypto_indices/mcap1000.py
+++ feeds/crypto_indices/mcap1000.py
@@ -1,5 +1,6 @@
 from feeds.data_feed import DataFeed
 import apis.coingecko as cgecko
+import apis.coinmarketcap as cmc
 from collections import deque 
 
 
@@ -8,18 +9,23 @@ class MCAP1000(DataFeed):
     ID = 2 
     HEARTBEAT = 180
     DATAPOINT_DEQUE = deque([], maxlen=100)
-    N = 1000
+    N = 10
 
     @classmethod
     def process_source_data_into_siwa_datapoint(cls):
         '''
+            Process data from multiple sources
         '''
-        market_data = cgecko.fetch_data_by_mcap(cls.N) 
-        if market_data is None:
-            return cls.DATAPOINT_DEQUE[-1]  #This should fail if DEQUE is empty
-        mcaps = sorted(list(market_data.keys()), reverse=True)
-        res =  sum(mcaps[:cls.N])
-        return res
+        res = []
+        for source in [cmc, cgecko]:
+            market_data = source.fetch_data_by_mcap(cls.N)
+            if market_data is None:
+                return cls.DATAPOINT_DEQUE[-1]  # This should fail if DEQUE is empty
+            mcaps = sorted(list(market_data.keys()), reverse=True)
+            res.append(sum(mcaps[:cls.N]))
+        # Take average of values from both sources
+        result = sum(res) / len(res)
+        return result
 
     @classmethod
     def create_new_data_point(cls):
