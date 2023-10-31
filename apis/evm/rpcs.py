edab2d77afc1acf2b12905ad9066488cd8b46634
deepsp94 <anantsp94@gmail.com>
2023-10-31 18:41:13 -0400

    Read keys from env and return all RPCs

--- apis/evm/rpcs.py
+++ apis/evm/rpcs.py
@@ -1,15 +1,43 @@
+import os
+
+
+# List of networks
+ARBITRUM_ONE = 'arbitrum_one'
+
+
+# List of providers
 ALCHEMY = 'alchemy'
 INFURA = 'infura'
 POKT = 'pokt'
 
 
-arbitrum_one_rpcs = {
-    ALCHEMY: 'https://arb-mainnet.g.alchemy.com/v2/',
-    INFURA: 'https://arb1.infura.io/v3/',
-    POKT: 'https://arbitrum-one.gateway.pokt.network/v1/lb/'
+# RPC URLs for each provider and each network
+RPCS = {
+    ARBITRUM_ONE: {
+        ALCHEMY: 'https://arb-mainnet.g.alchemy.com/v2/',
+        INFURA: 'https://arb1.infura.io/v3/',
+        POKT: 'https://arbitrum-one.gateway.pokt.network/v1/lb/'
+    }
 }
 
 
+def read_api_key(key_name):
+    # Read API keys from environment variables
+    try:
+        api_key = os.environ[key_name]
+    except KeyError:
+        print(f"{key_name} not set; skipping associated RPC URL.")
+    return api_key
+
+
+def append_api_key(provider):
+    key_name_suffix = '_api_key'
+    api_key = read_api_key(provider + key_name_suffix)
+    return api_key
+
+
 def get_rpc_urls(network):
-    if network == 'arbitrum-one':
-        return arbitrum_one_rpcs
+    rpcs = RPCS[network]
+    for provider, url in rpcs.items():
+        rpcs[provider] = url + append_api_key(provider)
+    return rpcs
