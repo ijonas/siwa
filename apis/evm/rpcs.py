9cdc7a8c45fab1542befa01a0e0eb673caab4ebd
deepsp94 <anantsp94@gmail.com>
2023-10-31 18:22:50 -0400

    RPC URLs

---
+++ apis/evm/rpcs.py
@@ -0,0 +1,10 @@
+ALCHEMY = 'alchemy'
+INFURA = 'infura'
+POKT = 'pokt'
+
+
+arbitrum_one_rpcs = {
+    ALCHEMY: 'https://arb-mainnet.g.alchemy.com/v2/',
+    INFURA: 'https://arb1.infura.io/v3/',
+    POKT: 'https://arbitrum-one.gateway.pokt.network/v1/lb/'
+}
