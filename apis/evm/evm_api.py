c9cba886c8b02b901b7f6b4c3a82b8ca4e35041b
deepsp94 <anantsp94@gmail.com>
2023-11-01 21:42:21 -0400

    Make class instantiation more general

--- apis/evm/evm_api.py
+++ apis/evm/evm_api.py
@@ -6,10 +6,20 @@ from rpcs import get_rpc_urls
 
 class EVM_API:
     def __init__(self,
-                 contract_addr: str,
-                 function_name: str,
-                 args: List[Any],
-                 rpc_urls: List[str]):
+                 rpc_urls: List[str],
+                 contract_addr: str = None,
+                 function_name: str = None,
+                 args: List[Any] = None,
+                 connect: bool = False,
+                 ):
+        self.connected = False
+        if connect:
+            self.connect(rpc_urls)
+        if contract_addr is None:
+            return
+        if contract_addr is not None and function_name is None:
+            raise Exception("Must provide function name if contract address "
+                            "is provided.")
         self.contract_addr = contract_addr
         self.abi = self.get_abi_from_file(contract_addr)
         self.function_name = function_name
@@ -20,11 +30,16 @@ class EVM_API:
         with open(f'apis/evm/abis/{addr}.json') as f:
             return json.load(f)
 
-    def get_values(self):
-        self.connect(self.rpc_urls)
+    def get_values(self, connect_every_time: bool = False):
+        # If connect_every_time is True, connect to a provider when this method
+        # is called. Also connect if not connected yet.
+        if connect_every_time or not self.connected:
+            self.connect(self.rpc_urls)
+        # Instantiate contract
         contract = self.web3.eth.contract(address=self.contract_addr,
                                           abi=self.abi)
         try:
+            # Call function
             func = getattr(contract.functions, self.function_name)
             result = func(*self.args).call()
             return result
@@ -42,6 +57,7 @@ class EVM_API:
                 web3_instance = Web3(Web3.HTTPProvider(url))
                 if web3_instance.isConnected():
                     self.web3 = web3_instance
+                    self.connected = True
                     break
             except Exception as e:
                 print(f"Failed to connect to {url}: {e}")
