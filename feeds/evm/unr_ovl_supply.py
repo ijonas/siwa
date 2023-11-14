4e3adddcc3a55e4c3aef7536a06fd20c167a059a
deepsp94 <anantsp94@gmail.com>
2023-11-14 14:22:19 -0500

    Add global variables and functions to class

--- feeds/evm/unr_ovl_supply.py
+++ feeds/evm/unr_ovl_supply.py
@@ -4,71 +4,65 @@ from apis.evm import evm_api, rpcs
 from web3 import Web3
 from feeds.data_feed import DataFeed
 import time
-from collections import deque
-
-# Constants
-SUBGRAPH_URL = 'https://api.studio.thegraph.com/query/46086/overlay-v2-subgraph-arbitrum/version/latest'  # NOQA E501
-MULTICALL_ADDRESS = '0x842eC2c7D803033Edf55E478F461FC547Bc54EB2'
-STATE_ADDRESS = '0xC3cB99652111e7828f38544E3e94c714D8F9a51a'
-
-# Initialize EVM_API instances
-rpc_urls = list(rpcs.get_rpc_urls(rpcs.ARBITRUM_ONE).values())
-multicall_api = evm_api.EVM_API(rpc_urls, MULTICALL_ADDRESS,
-                                'aggregate', connect=True)
-overlay_api = evm_api.EVM_API(rpc_urls, STATE_ADDRESS, connect=True)
-
-
-# Pagination query function
-def query_subgraph(first, skip):
-    query = '''
-    {
-        builds(first: %s, skip: %s) {
-            collateral
-            id
-            position {
-                currentOi
-                fractionUnwound
-            }
-            owner {
-                id
-            }
-        }
-    }
-    ''' % (first, skip)
-    response = requests.post(SUBGRAPH_URL, json={'query': query})
-    data = response.json().get('data', {}).get('builds', [])
-    return data
-
-
-# Function to encode calls for multicall
-def get_value_calls(data_frame):
-    calls = []
-    for _, row in data_frame.iterrows():
-        call_data = overlay_api.web3.eth.contract(
-            address=Web3.toChecksumAddress(STATE_ADDRESS),
-            abi=overlay_api.abi
-        ).encodeABI(
-            fn_name='value',
-            args=[
-                Web3.toChecksumAddress(row['market']),
-                Web3.toChecksumAddress(row['owner.id']),
-                int(row['position_id'])
-            ]
-        )
-        calls.append((STATE_ADDRESS, call_data))
-    return calls
-
-
-def chunked_multicall(calls, chunk_size=100):
-    # Break the calls into chunks of specified size
-    for i in range(0, len(calls), chunk_size):
-        yield calls[i:i + chunk_size]
 
 
 class UnrealisedOVLSupply(DataFeed):
     NAME = 'unr_ovl_supply'
     ID = 10
     HEARTBEAT = 12
+    SUBGRAPH_URL = 'https://api.studio.thegraph.com/query/46086/overlay-v2-subgraph-arbitrum/version/latest'  # noqa: E501
+    MULTICALL_ADDRESS = '0x842eC2c7D803033Edf55E478F461FC547Bc54EB2'
+    STATE_ADDRESS = '0xC3cB99652111e7828f38544E3e94c714D8F9a51a'
+
+    rpc_urls = list(rpcs.get_rpc_urls(rpcs.ARBITRUM_ONE).values())
+    multicall_api = evm_api.EVM_API(rpc_urls, MULTICALL_ADDRESS,
+                                    'aggregate', connect=True)
+    overlay_api = evm_api.EVM_API(rpc_urls, STATE_ADDRESS, connect=True)
+
+    @classmethod
+    def query_subgraph(cls, first, skip):
+        query = f'''
+        {{
+            builds(first: {first}, skip: {skip}) {{
+                collateral
+                id
+                position {{
+                    currentOi
+                    fractionUnwound
+                }}
+                owner {{
+                    id
+                }}
+            }}
+        }}
+        '''
+        response = requests.post(cls.SUBGRAPH_URL, json={'query': query})
+        data = response.json().get('data', {}).get('builds', [])
+        return data
+
+    @classmethod
+    def get_value_calls(cls, data_frame):
+        calls = []
+        for _, row in data_frame.iterrows():
+            call_data = cls.overlay_api.web3.eth.contract(
+                address=Web3.toChecksumAddress(cls.STATE_ADDRESS),
+                abi=cls.overlay_api.abi
+            ).encodeABI(
+                fn_name='value',
+                args=[
+                    Web3.toChecksumAddress(row['market']),
+                    Web3.toChecksumAddress(row['owner.id']),
+                    int(row['position_id'])
+                ]
+            )
+            calls.append((cls.STATE_ADDRESS, call_data))
+        return calls
+
+    @staticmethod
+    def chunked_multicall(calls, chunk_size=100):
+        # Break the calls into chunks of specified size
+        for i in range(0, len(calls), chunk_size):
+            yield calls[i:i + chunk_size]
 
     @classmethod
     def process_source_data_into_siwa_datapoint(cls):
@@ -76,7 +70,7 @@ class UnrealisedOVLSupply(DataFeed):
         skip = 0
         all_data = []
         while True:
-            data = query_subgraph(1000, skip)
+            data = cls.query_subgraph(1000, skip)
             if not data:
                 break
             all_data.extend(data)
@@ -88,24 +82,21 @@ class UnrealisedOVLSupply(DataFeed):
         df['position_id'] = df['position_id'].apply(lambda x: int(x, 16))
 
         # Get the value calls for multicall
-        value_calls = get_value_calls(df)
-        value_calls = [value_calls[0] for call in value_calls]
+        value_calls = cls.get_value_calls(df)
+        # value_calls = [value_calls[0] for call in value_calls]
         all_values = []
         counter = 0
-        latest_block = overlay_api.web3.eth.get_block('latest')
+        latest_block = cls.overlay_api.web3.eth.get_block('latest')
         latest_block = latest_block['number']
-        for chunk in chunked_multicall(value_calls, 755):
+        for chunk in cls.chunked_multicall(value_calls, 755):
             # Set multicall API function name and arguments for the chunk
-            multicall_api.args = [chunk]
+            cls.multicall_api.args = [chunk]
 
             # Execute multicall for the chunk
-            # if counter == 6:
-            #     breakpoint()
-            response = multicall_api.get_values(block=latest_block)
+            response = cls.multicall_api.get_values(block=latest_block)
             counter += 1
             print(f"Processing chunk {counter}")
-            print("Sleeping for 0.2 seconds...")
-            time.sleep(1)
+            time.sleep(0.2)
 
             # Decode the response data for the chunk
             chunk_values = [int.from_bytes(val, 'big') for val in response[1]]
